3f4c596b52b7c24f7350f0c8b7c698d0ac88a510
Evadne Wu <ev@monoceroi.com>
2010-07-12 10:11:47 +0800

    updates header, rearranges code snippets, and defines flag mono.arrayAdditions

--- lib.mono.array.js
+++ lib.mono.array.js
@@ -5,73 +5,91 @@
 
 
 
-if (!Array.prototype.supersets) Array.prototype.supersets = function (anArray) {
 
-	for (plausibleItemIndex in this) {
+	mono.arrayAdditions = true;
 
-		if (!this.hasOwnProperty(plausibleItemIndex)) continue; 
-		if (this[plausibleItemIndex] != anArray[0]) continue; 
-		
-		if ((this.length - plausibleItemIndex) < anArray.length) break;
-		
-		var subArrayMatched = true;	//	assertion
 
-		for (itemIndexDelta in anArray) {
-		
-			if (!anArray.hasOwnProperty(itemIndexDelta)) continue; 
-		
-			var comparingElement = this[Number(plausibleItemIndex) + Number(itemIndexDelta)];
-			var comparedElement = anArray[itemIndexDelta];
-		
-			if (comparingElement != comparedElement) {
+
+
+
+//	! 
+//	!Set Operation & Introspection
+
+	Array.prototype.supersets = function (anArray) {
+	
+		for (plausibleItemIndex in this) {
+	
+			if (!this.hasOwnProperty(plausibleItemIndex)) continue; 
+			if (this[plausibleItemIndex] != anArray[0]) continue; 
 			
-				subArrayMatched = false;
-				break;
+			if ((this.length - plausibleItemIndex) < anArray.length) break;
+			
+			var subArrayMatched = true;	//	assertion
+	
+			for (itemIndexDelta in anArray) {
+			
+				if (!anArray.hasOwnProperty(itemIndexDelta)) continue; 
+			
+				var comparingElement = this[Number(plausibleItemIndex) + Number(itemIndexDelta)];
+				var comparedElement = anArray[itemIndexDelta];
+			
+				if (comparingElement != comparedElement) {
+				
+					subArrayMatched = false;
+					break;
+				
+				}
 			
 			}
-		
+			
+			if (subArrayMatched) return true;
+			
 		}
-		
-		if (subArrayMatched) return true;
-		
-	}
-
-	return false;
-
-}
-
-
-
-
-
-if (!Array.prototype.pushIfNotEmpty) Array.prototype.pushIfNotEmpty = function (object) {
-
-	if (typeof object == 'undefined') return;
-	if ((object && object.length) <= 0) return;
 	
-	this.push(object);
-
-}
-
-
-
-
-
-if (!Array.prototype.containsObject) Array.prototype.hasObject = function (object, caseInsensitive) {
-
-	for (var i = 0; i < this.length; i++) {
-
-		if (this[i] == object) return true;
-		
-		if (!(caseInsensitive && (typeof this[i] == 'string') && (typeof object == 'string'))) continue;
-		
-		return (this[i].match(RegExp(object, "i")) != null);
-
+		return false;
+	
 	}
 
-	return false;
 
-}
+
+
+
+	if (!Array.prototype.containsObject) Array.prototype.hasObject = function (object, caseInsensitive) {
+	
+		for (var i = 0; i < this.length; i++) {
+	
+			if (this[i] == object) return true;
+			
+			if (!(caseInsensitive && (typeof this[i] == 'string') && (typeof object == 'string'))) continue;
+			
+			return (this[i].match(RegExp(object, "i")) != null);
+	
+		}
+	
+		return false;
+	
+	}
+
+
+
+
+
+//	! 
+//	!Enqueuing
+
+	Array.prototype.pushIfNotEmpty = function (object) {
+	
+		if (typeof object == 'undefined') return;
+		if ((object && object.length) <= 0) return;
+		
+		this.push(object);
+	
+	}
+
+
+
+
+
 
 
 
